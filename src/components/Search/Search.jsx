7499e822557a385e10649e02bf1fcb2b5018e9df
mobilesmile5 <colomba@wwu.edu>
2025-02-10 16:48:16 -0800

    Working example of how to use colortheme in search.css, and added itenerary component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,8 +5,14 @@ import {
   GeoapifyGeocoderAutocomplete,
 } from "@geoapify/react-geocoder-autocomplete";
 
+// import { ColorThemeProvider, colorTheme } from './ColorTheme/ColorTheme.jsx';
+import { userColorTheme } from "../ColorTheme/ColorTheme";
+import './Search.css';
+
+
 const Search = () => {
   const [query, setQuery] = useState("");
+  const { theme } = userColorTheme();
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
@@ -26,11 +32,23 @@ const Search = () => {
     <>
       <GeoapifyContext apiKey="e96aed6945a84bcbb46d19d02f54e66c">
         <div>
+          {/* below is the textfield used in the search */}
           <GeoapifyGeocoderAutocomplete
             placeholder="Enter a location or address"
             type="city"
             value={query}
+            // onChange={handleInputChange} 
+            // onSelect={handlePlaceSelect} 
           />
+
+          {/* Button used to submit search */}
+          <button
+            className="search-button"
+            onClick={handleSearch} // Trigger search on click of button
+          >
+            Search
+          </button>
+
         </div>
       </GeoapifyContext>
     </>
--- src/pages/TripsPage.jsx
+++ src/pages/TripsPage.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import Search from "../components/Search";
+import Search from "../components/Search/Search.jsx";
+import { ColorThemeProvider, colorTheme } from '../components/ColorTheme/ColorTheme.jsx';
+
 
 const TripsPage = () => {
   return (
-    <div>
-      <form className="">
-        <label>
-          Location
-          <Search />
-        </label>
-        <label>
-          Description
-          <input placeholder="Input your activity here" />
-        </label>
-      </form>
-    </div>
+    <ColorThemeProvider>
+      <div>
+        <form className="">
+          <label>
+            Location
+            <Search />
+          </label>
+          <label>
+            Description
+            <input placeholder="Input your activity here" />
+          </label>
+        </form>
+      </div>
+    </ColorThemeProvider>
   );
 };
 
