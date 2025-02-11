a4dd21a62aa21f3beefaf38404c89bec3635f919
schaefm2 <schaefm2@wwu.edu>
2025-02-10 16:35:27 -0800

    Navigation styling

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import MaterialSymbolsTravelExplore from "../assets/material-symbols_travel-explore.svg";
+import "./NavStyle.css";
+
+const textWrapper =
+  "relative w-fit mt-[-1.00px] [font-family:'Montserrat-Medium',Helvetica] font-medium text-black text-base tracking-[0] leading-4 whitespace-nowrap";
 
 const Navigation = () => {
   return (
     <nav className=" w-[1404px] h-[50px]">
-      <div className="inline-flex items-center gap-10 absolute top-[18px] left-[545px] ">
-        <div className="flex space-x-10">
-          <NavLink to="/">Home</NavLink>
-        </div>
-        <div>
-          <NavLink to="/hotels">Hotels</NavLink>
-        </div>
-        <div>
-          <NavLink to="/flights">Flights</NavLink>
-        </div>
-        <div>
-          <NavLink to="/rentals">Rentals</NavLink>
-        </div>
-        <div>
-          <NavLink to="/trips">Trips</NavLink>
+      <div className="inline-flex items-center gap-10 absolute top-[18px] left-[498px]">
+        <NavLink to="/">
+          <div className={textWrapper}> Home</div>
+        </NavLink>
+
+        <NavLink to="/hotels">
+          <div className={textWrapper}>Hotels</div>
+        </NavLink>
+        <NavLink to="/flights">
+          <div className={textWrapper}>Flights</div>
+        </NavLink>
+        <NavLink to="/rentals">
+          <div className={textWrapper}>Rentals</div>
+        </NavLink>
+        <NavLink to="/trips">
+          <div className={textWrapper}>Trips</div>
+        </NavLink>
+      </div>
+      <div className="inline-flex items-center gap-1.5 absolute top-0 left-0">
+        <img
+          src={MaterialSymbolsTravelExplore}
+          className="!relative !w-[50px] !h-[50px]"
+        />
+        <div className="relative w-fit [font-family:'Montserrat-SemiBold',Helvetica] font-semibold text-black text-2xl tracking-[0] leading-6 whitespace-nowrap">
+          Crusader Travels
         </div>
       </div>
     </nav>
