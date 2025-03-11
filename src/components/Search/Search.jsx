78f84ef09eef1614fbaf69808fcd77e44c07e486
schaefm2 <schaefm2@wwu.edu>
2025-03-11 16:21:31 -0700

    Search component gives the place, some trip updates

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,30 +5,23 @@ import {
   GeoapifyGeocoderAutocomplete,
 } from "@geoapify/react-geocoder-autocomplete";
 
-const Search = ({ placeholder, className, value, onPlaceSelect }) => {
-  const [query, setQuery] = useState(value);
-
-  const handleInputChange = (event) => {
-    setQuery(event.target.value);
-  };
-
-  const handlePlaceSelect = (value) => {
-    console.log("handlePlaceSelect called with value: ", value);
-    if (onPlaceSelect) {
-      onPlaceSelect(value);
+const Search = ({ placeholder, className, onSelect }) => {
+  const onPlaceSelect = (place) => {
+    console.log("onPlaceSelect fired");
+    if (place) {
+      console.log("Place selected:", place);
+      onSelect(place);
+    } else {
+      console.log("No place data received.");
     }
   };
-
   return (
     <GeoapifyContext apiKey="e96aed6945a84bcbb46d19d02f54e66c">
       <div className={className}>
         <GeoapifyGeocoderAutocomplete
           placeholder={placeholder}
           type="city"
-          value={query}
-          onChange={(e) => handleInputChange(e)}
-          onPlaceSelect={(value) => handlePlaceSelect(value)}
-          className="rounded-full"
+          placeSelect={onPlaceSelect}
         />
       </div>
     </GeoapifyContext>
